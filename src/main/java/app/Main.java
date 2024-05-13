e1aa7b35fbc787716dccad429ea34a7cf2363d8f
Nazar <nazarpavl09102004@gmail.com>
2024-05-13 21:46:35 +0200

    add farengates to celsius  convention

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,5 +3,21 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App.");
+
+        double f = 10;
+        double celFromFar = convFarToCel(f) ;
+        System.out.println("Result is " + celFromFar + " celsius.");
+
+        double c = 12;
+        double farFromCel = convCelToFar(c) ;
+        System.out.println("Result is " + celFromFar + " farengates.");
+    }
+
+    public static double convFarToCel(double f) {
+        return (f-32)/5*9;
+    }
+
+    public static double convCelToFar(double f) {
+        return f * 9 / 5 + 32;
     }
 }
